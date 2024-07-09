a4c55027c21146864215a4bd6215d143f6321973
xmu-csnoob <395110618@qq.com>
2024-07-09 11:23:59 +0800

    add type.h

--- main.c
+++ main.c
@@ -1,4 +1,39 @@
 #include <stdio.h>
-int main(){
-    printf("123");
+#include "type.h"
+#include "memory.h"
+
+void print_memory_content(int start, int end) {
+    for (int i = start; i < end; i++) {
+        printf("memory[%d] = 0x%02X\n", i, memory[i]);
+    }
+}
+
+int main() {
+    init_memory();
+
+    printf("Initial memory state:\n");
+    print_memory_content(0, 16); // 打印前16个字节
+
+    int int_data = 123817;
+    pointer int_ptr = create_pointer(0, INTEGER);
+    assign_pointer(int_ptr, &int_data);
+
+    char char_data = 'A';
+    pointer char_ptr = create_pointer(8, CHAR);
+    assign_pointer(char_ptr, &char_data);
+
+    printf("\nMemory state after assignments:\n");
+    print_memory_content(0, 16);
+
+    unsigned char* retrieved_int_data = solve_pointer(int_ptr);
+    unsigned int int_value = readInteger(retrieved_int_data);
+    printf("\nRetrieved integer value: %d\n", int_value);
+    free(retrieved_int_data);
+
+    unsigned char* retrieved_char_data = solve_pointer(char_ptr);
+    unsigned char char_value = readChar(retrieved_char_data);
+    printf("Retrieved char value: %c\n", char_value);
+    free(retrieved_char_data);
+
+    return 0;
 }
--- type.h
+++ type.h
@@ -1,26 +1,100 @@
 #ifndef TYPE_H
 #define TYPE_H
-#include "stdio.h"
+
+#include <stdio.h>
+#include <stdlib.h>
 #include "memory.h"
-typedef enum{
-    INTERGER,
+
+#define INTEGER_BYTES 4
+#define CHAR_BYTES 1
+
+typedef enum {
+    INTEGER,
     CHAR,
     NULLTYPE
 } type;
 
-typedef struct{
+typedef struct {
     size_t address;
     type t;
 } pointer;
 
-pointer create_pointer(size_t address, type t){
+pointer create_pointer(size_t address, type t) {
     pointer p;
-    if(address >=0 && address < MEMORY_SIZE){
+    if (address >= 0 && address < MEMORY_SIZE) {
         p.address = address;
         p.t = t;
-    }else{
-        p.address = NULL;
+    } else {
+        p.address = 0;
         p.t = NULLTYPE;
     }
     return p;
 }
+
+void assign_pointer(pointer p, void* data) {
+    if (p.address >= 0 && p.address < MEMORY_SIZE) {
+        if (p.t == INTEGER) {
+            if (p.address + INTEGER_BYTES <= MEMORY_SIZE) {
+                unsigned char int_data[INTEGER_BYTES];
+                int value = *((int*)data);
+                int_data[0] = (value >> 24) & 0xFF;
+                int_data[1] = (value >> 16) & 0xFF;
+                int_data[2] = (value >> 8) & 0xFF;
+                int_data[3] = value & 0xFF;
+                assign_memory(p.address, int_data, INTEGER_BYTES);
+            } else {
+                printf("Warning: address out of bounds for INTEGER assignment\n");
+            }
+        } else if (p.t == CHAR) {
+            if (p.address + CHAR_BYTES <= MEMORY_SIZE) {
+                unsigned char char_data = *((unsigned char*)data);
+                assign_memory(p.address, &char_data, CHAR_BYTES);
+            } else {
+                printf("Warning: address out of bounds for CHAR assignment\n");
+            }
+        } else {
+            printf("Warning: invalid type for pointer assignment\n");
+        }
+    } else {
+        printf("Warning: invalid address for pointer assignment\n");
+    }
+}
+
+unsigned char* solve_pointer(pointer p) {
+    unsigned char *data = NULL;
+    if (p.address >= 0 && p.address < MEMORY_SIZE) {
+        if (p.t == INTEGER) {
+            data = (unsigned char*)malloc(INTEGER_BYTES);
+            unsigned char *mem_data = access_memory(p.address);
+            if (mem_data != NULL) {
+                memcpy(data, mem_data, INTEGER_BYTES);
+            }
+        } else if (p.t == CHAR) {
+            data = (unsigned char*)malloc(CHAR_BYTES);
+            unsigned char *mem_data = access_memory(p.address);
+            if (mem_data != NULL) {
+                data[0] = mem_data[0];
+            }
+        }
+    }
+    return data;
+}
+
+unsigned int readInteger(unsigned char* data) {
+    if (data == NULL) {
+        printf("Warning: data is NULL\n");
+        return 0;
+    }
+    unsigned int combined = (data[0] << 24) | (data[1] << 16) | (data[2] << 8) | data[3];
+    return combined;
+}
+
+unsigned char readChar(unsigned char* data) {
+    if (data == NULL) {
+        printf("Warning: data is NULL\n");
+        return 0;
+    }
+    return *data;
+}
+
+#endif
