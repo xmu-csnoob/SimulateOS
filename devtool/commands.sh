00542e7522a2087474a80d3e7a6ff8df0c39bdc6
xmu-csnoob <395110618@qq.com>
2024-08-09 17:48:22 +0800

    universal version of commands.sh

--- devtool/commands.sh
+++ devtool/commands.sh
@@ -1,45 +1,40 @@
 #!/bin/bash
 
-function mkk() {
+# 编译 Debug 版本
+function mkd() {
     mkdir -p build
     cd build
-    cmake ..
-    make kernel
+    cmake -DCMAKE_BUILD_TYPE=Debug ..
+    make ${1:+$1}  # 如果有参数传入则make该参数，否则不传参数直接make
     cd ..
 }
 
-function mkt() {
+# 编译 Release 版本
+function mkr() {
     mkdir -p build
     cd build
-    cmake ..
-    make test
+    cmake -DCMAKE_BUILD_TYPE=Release ..
+    make ${1:+$1}  # 如果有参数传入则make该参数，否则不传参数直接make
     cd ..
 }
 
-function mk() {
-    mkk
-    mkt
-}
-
+# 清理构建目录并删除当前目录下的所有可执行文件
 function mc() {
-    if [ -f "kernel" ]; then
-        rm kernel
-    fi
-
-    if [ -f "test" ]; then
-        rm test
-    fi
-
+    # 删除build目录
     if [ -d "build" ]; then
-        rm -r build
+        rm -rf build
     fi
+
+    # 删除当前目录下的所有可执行文件
+    find . -maxdepth 1 -type f -executable -exec rm -f {} \;
 }
 
-
+# 设置环境变量
 function setenv() {
     export $1=$2
 }
 
+# 设置日志级别
 function lg(){
     export LOG_LEVEL=$1
 }
